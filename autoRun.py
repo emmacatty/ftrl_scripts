bfc1ef14abb503d62168b4109e76bf002716b35c
xuwenwen <xuwenwen@xiaomi.com>
2017-02-23 10:17:00 +0800

    add

---
+++ autoRun.py
@@ -0,0 +1,125 @@
+import sys,os
+import time
+from multiprocessing import Process
+from multiprocessing import Pool
+
+SECOND_PER_DAY = 24 * 60 * 60
+
+def exec_cmd(cmd):
+    print "START %s" % cmd
+    ret = os.system(cmd)
+#time.sleep(3)
+    if ret == 1:
+        print "ERROR %s" % cmd
+        return False
+    return True
+
+def offline_fea(i):
+    cmd = 'sh run_phone.sh %s' % i
+    return exec_cmd(cmd)
+
+def download_data(dataPath, fileName):
+    os.chdir("../data")
+    targetDir = dataPath + fileName
+    cmd = "hadoop --cluster c3prc-hadoop dfs -get " + targetDir
+    ret = exec_cmd(cmd)
+    os.chdir("../scripts")
+    return ret
+
+def model_filter(infile, outfile):
+    lite_model = open(outfile, 'w')
+    for line in open(infile, 'r'):
+        if not line:
+            break
+        else:
+            if line.split('\t')[1] != '0':
+                lite_model.write(line)            
+    lite_model.close()                                        
+
+def predict_auc(filename, flag):
+    cmd = "cat ../data/" + filename + "/* | gzip -d |"
+    ds = time.strftime("%Y%m%d", time.localtime(time.time() - 2*SECOND_PER_DAY))
+    #model filter 0s
+    model_filter("model."+flag+".2", "model."+flag+"."+ds)
+    cmd += " ./ftrl_predict model." + flag + "." + ds + " 10 res." + flag +"."+ds
+    if exec_cmd(cmd):
+        cmd = "rm -rf ../data" + filename
+        if exec_cmd(cmd):
+            cmd = "cat res." + flag + "." + ds + " | ./AUC.py &> auc." + flag + "." + ds
+            return exec_cmd(cmd)
+    return False
+    
+def train_or_predict(round, filename, flag, daySpan):
+    if round == 1:
+        # ds = time.strftime("%Y%m%d", time.localtime(time.time() - 2*SECOND_PER_DAY))
+        # #model filter 0s
+        # model_filter("model."+flag+".2", "model."+flag+"."+ds)
+        # cmd += " ./ftrl_predict model." + flag + "." + ds + " 10 res." + flag +"."+ds
+        predict_auc(filename, flag)
+    else:
+        cmd = "cat ../data/" + filename + "/* | gzip -d | ./ftrl_train"
+        if round < daySpan + 1:
+            cmd += " -im model." + flag + "." + str(round+1)
+        cmd += " -m model." + flag + "." + str(round) + " -l1 5 -core 10 -bias 1"
+        if exec_cmd(cmd):
+            #drop data
+            cmd = "rm -rf ../data/" + filename
+            return exec_cmd(cmd)
+        return False
+    
+def train_core(daySpan, dataPath, flag):
+    for i in range(daySpan + 1, 0, -1):
+        ds = time.strftime("%Y%m%d", time.localtime(time.time() - i * SECOND_PER_DAY))
+        fileName = "date=%s_CPC_" % ds + flag
+        if not download_data(dataPath, fileName):
+            break
+        if not train_or_predict(i, fileName, flag, daySpan):
+            break
+
+status = []
+def err_callback(arg):
+    if arg:
+        status.append(0)
+    else:
+        status.append(1)
+        
+        
+def train(daySpan, dataPath):
+    # step1: offline fea extract
+    # pool = Pool(min(24, daySpan + 1))
+    # for i in range(1, daySpan + 2):
+    #     pool.apply_async(func=offline_fea, args=(i, ), callback=err_callback)
+    # pool.close()
+    # pool.join()
+    # pool.terminate()
+    # if sum(status) != 0:
+    #     print "offline fea extract faild: %s" % sum(status)
+    #     return
+
+    for i in range(1, daySpan + 2):
+        offline_fea(i)
+    
+    # p_list=[]
+    # for i in range(1, daySpan + 2):
+    #     p = Process(target=offline_fea, args=(i, ))
+    #     p.daemon = True
+    #     p_list.append(p)
+    # for p in p_list:
+    #     p.start()
+    # for p in p_list:
+    #     p.join()
+
+    # step2: download data & train & predict
+    p_base = Process(target=train_core, args=(daySpan, dataPath, "base_feature", ))
+    p_base.start()
+    p_opt = Process(target=train_core, args=(daySpan, dataPath, "phone_video", ))
+    p_opt.start()
+    p_base.join()
+    p_opt.join()
+
+    # step3: auc
+
+            
+if __name__ == '__main__':
+    dataPath = '/user/h_miui_ad/wwxu/test/phone_context/'
+    train(2, dataPath)
---
+++ run_phone.sh
@@ -0,0 +1,93 @@
+#!/bin/sh
+
+#source /home/work/.bashrc
+#source /home/work/.bash_profile
+#git pull
+#mvn clean package -U
+#set
+#queue="miui_recommendation"
+#queue="root.default"
+queue="root.service.miui_group.miui_ad"
+#queue="root.production.miui_group.miui_ad.queue_1"
+num_executor="200"
+master="yarn-cluster"
+#master="local[*]"
+
+#HADOOP_HOME="/home/work/tools/infra-client"
+SPARK_HOME="/home/work/tools/infra-client"
+
+#JAR path
+JAR_PATH="/home/work/wwxu/exp/jars/feature_extractor-1.0-SNAPSHOT.jar"
+
+#input path
+date=`date -d "$1 days ago" +%Y%m%d`
+hist_date=`date -d "$1 days ago" +%Y_%m_%d`
+year=`date -d "$1 days ago" +%Y`
+month=`date -d "$1 days ago" +%m`
+day=`date -d "$1 days ago" +%d`
+echo $year, $month, $day, $hist_date, $date
+
+#adInfoPath=/user/h_miui_ad/feed_ads/yidian/data/ad_info_target
+#adInfoPath=/user/h_miui_ad/feed_ads/yidian/data/ad_info_target_tag
+adInfoPath=/user/h_miui_ad/ad_prediction_service_corpus/ad_corpus/ad_info_target_tag_emiv2
+userInfoPath=/user/h_data_platform/platform/miuiads/ml_dau_profile/date=${date}/
+historicalInfoPath=/user/h_miui_ad/ad_prediction_service_corpus/history_corpus/${hist_date}*
+adEventPath=/user/h_data_platform/platform/miuiads/ad_historical_event/date=${date}/
+adLogV2Path=/user/h_scribe/miuiads/miuiads_ad_log_v2/year=${year}/month=${month}/day=${day}
+
+while [ 1 == 1 ]
+do
+    hadoop --cluster c3prc-hadoop fs -test -e /user/h_data_platform/platform/miuiads/ml_dau_profile/date=${date}/
+    if [ $? == 0 ]
+    then
+        break
+    fi
+    echo "File /user/h_data_platform/platform/miuiads/ml_dau_profile/date=${date}/ not exist, wait"
+    sleep 60
+done
+
+while [ 1 == 1 ]
+do
+    hadoop --cluster c3prc-hadoop fs -test -e /user/h_data_platform/platform/miuiads/ad_historical_event/date=${date}/
+    if [ $? == 0 ]
+    then
+        break
+    fi
+    echo "File /user/h_data_platform/platform/miuiads/ad_historical_event/date=${date}/ not exist, wait"
+    sleep 60
+done
+
+#output path
+outputfiles=/user/h_miui_ad/wwxu/test/phone_context/date=${date}
+
+hadoop --cluster c3prc-hadoop fs -rmr ${outputfiles}*
+
+#main class
+class=Job.PhoneVideoWithClient
+
+echo "submit spark"
+spark-submit \
+    --cluster c3prc-hadoop \
+    --class $class \
+    --master $master \
+    --num-executors $num_executor \
+    --queue $queue \
+    --driver-memory 6g \
+    --executor-memory 4g \
+    --conf spark.dynamicAllocation.enabled=true \
+    --conf spark.shuffle.service.enabled=true \
+    --conf spark.dynamicAllocation.minExecutors=20 \
+    --conf spark.dynamicAllocation.maxExecutors=400 \
+    --conf spark.yarn.driver.memoryOverhead=896 \
+    --conf spark.memory.fraction=0.3 \
+    $JAR_PATH $adInfoPath $userInfoPath $historicalInfoPath $adEventPath $adLogV2Path $outputfiles
+
+if [ $? == 0 ]
+then
+#    echo "PhoneVideoWithClient Feature Extractor Done" | mail -s "PhoneVideoWithClient Feature Extractor Done" "wuhonggang@xiaomi.com,yanming@xiaomi.com"
+    echo "PhoneVideoWithClient Feature Extractor Done" | mail -s "PhoneVideoWithClient Feature Extractor Done" "xuwenwen@xiaomi.com"
+    exit 0
+fi
+#echo "PhoneVideoWithClient Feature Extractor Failed" | mail -s "PhoneVideoWithClient Feature Extractor Failed" "wuhonggang@xiaomi.com,yanming@xiaomi.com"
+echo "PhoneVideoWithClient Feature Extractor Failed" | mail -s "PhoneVideoWithClient Feature Extractor Failed" "xuwenwen@xiaomi.com"
+
